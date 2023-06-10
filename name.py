c19330303c239d8816591061addbecdf919558a1
Mardon1207 <mardonsultonov8@gmail.com>
2023-06-10 23:35:11 +0500

    name

--- name.py
+++ name.py
@@ -1,9 +1,12 @@
 import requests
 from randommer import Randommer
-
+randomer=Randommer()
 
 class Name(Randommer):
-    def get_name(self, api_key: str, nameType: str, quantity: int) -> list:
+    def __init__(self,api_key) -> None:
+        self.api_key=api_key
+
+    def get_name(self, nameType: str, quantity: int) -> list:
         '''get name
 
         Args:
@@ -14,9 +17,19 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        base_url=randomer.base_url      
+        url=base_url+"Name"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        payload={
+            "nameType":nameType,
+            "quantity":quantity
+        }
+        r=requests.get(url,params=payload, headers=heardes)
+        return r.json()
     
-    def get_name_suggestions(self, api_key: str, startingWords: str) -> list:
+    def get_name_suggestions(self, startingWords: str) -> list:
         '''get name suggestions
 
         Args:
@@ -26,9 +39,18 @@ class Name(Randommer):
         Returns:
             list: list of name suggestions
         '''
-        pass
+        base_url=randomer.base_url      
+        url=base_url+"Name/Suggestions"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        payload={
+            "startingWords":startingWords
+        }
+        r=requests.get(url,params=payload, headers=heardes)
+        return r.json()
     
-    def get_name_cultures(self, api_key: str) -> list:
+    def get_name_cultures(self) -> list:
         '''get available cultures
 
         Args:
@@ -37,4 +59,13 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        base_url=randomer.base_url      
+        url=base_url+"Name/Cultures"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        r=requests.get(url, headers=heardes)
+        return r.json()
+key="f1ab06cd2da14928a4f4299e85162d76"
+name=Name(key)
+print(name.get_name_cultures())
